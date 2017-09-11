994c69837db932cc9cfbf82a8c0376506c1d06e4
Kevin Yuan <yuanlin@zeaho.com>
2017-09-11 13:10:24 +0800

    finish file upload

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var upload = require('../middleware/upload');
 
 // 上传单个文件
-router.post('/single', function (req, res, next) {
+router.post('/single', upload.single('single'), function (req, res, next) {
+    console.log(req.file);
     res.send({ ret_code: '0' });
 });
 
