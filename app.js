ef86bdf1beb950d7b1bfb8e1074180182a6cc695
Kevin Yuan <yuanlin@zeaho.com>
2017-09-11 14:07:45 +0800

    add cors

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var cors = require('cors');
+app.use(cors());
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
