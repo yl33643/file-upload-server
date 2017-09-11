db812e1908f5c833970cb6a3dd2abe6f20f25579
Kevin Yuan <yuanlin@zeaho.com>
2017-09-11 13:06:28 +0800

    add multer

---
+++ middleware/upload.js
@@ -0,0 +1,29 @@
+var fs = require('fs');
+var multer = require('multer');
+
+// Step 1: 新建upload
+var createFolder = function (folder) {
+    try {
+        fs.accessSync(folder);
+    } catch (e) {
+        fs.mkdirSync(folder);
+    }
+};
+
+var uploadFolder = './upload/';
+
+createFolder(uploadFolder);
+
+// Step 2: 配置multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadFolder);
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+var upload = multer({ storage: storage });
+
+module.exports = upload;
